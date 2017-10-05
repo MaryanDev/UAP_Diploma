997cefac866760b2faa6b2d0ea65a5e5d7cb16f7
Mayher Maryan <mr.maykher@gmail.com>
2017-10-05 16:27:14 +0300

    finally added angular 1.6

--- PFSC/PFSC.Web/Startup.cs
+++ PFSC/PFSC.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
