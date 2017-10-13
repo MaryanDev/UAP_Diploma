3ba15b972665acab600f295a8518327992cb97ca
Mayher Maryan <mr.maykher@gmail.com>
2017-10-13 17:21:16 +0300

    added second project to fit in angular 2

---
+++ PFSC/PFSC.WebUI/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PFSC.Entities.PFSC_DBContext;
+using PFSC.Services.Abstract.Security;
+using PFSC.Services.Concrete.Security;
+
+namespace PFSC_WebUI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PfscDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("PFSC_Connection")));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = new PathString("/Account/Login");
+                options.ExpireTimeSpan = new TimeSpan(1, 0, 0);
+            });
+
+            services.AddTransient<IPfscAuthenticationService, PfscAuthenticationService>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+
+            app.UseMvc(routes =>
+            {
+
+                routes.MapRoute("login", "login", defaults: new { controller = "Account", action = "Login" });
+                routes.MapRoute("register", "register", defaults: new { controller = "Account", action = "Register" });
+                routes.MapRoute("search", "search/{criteria?}", defaults: new { controller = "Search", action = "Index" });
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
